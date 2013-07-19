85b75bddc36b3d6f21c321ba1ee0cef3325c738c
Alex Mark <ajmark@andrew.cmu.edu>
2013-07-19 11:19:41 -0400

    Refactored home/index

---
+++ includes/homecontent.php
@@ -0,0 +1,106 @@
+<div id='main'>
+      <div class="text-wrap">
+        <h1 class='intro'>
+          Just call me <span>Alex</span>.
+        </h1>
+        <h2 class='text-body'>I am a Carnegie Mellon student, studying Information Systems and Human Computer Interaction. I aspire to work in the UX field through the use of my skills as a software developer and my affinity for design and interfaces. Check out some of my work below. </h2>
+        <br>
+			</div>
+			<div id='portfolio-main' class='text-body'>
+				<h1>Projects<span>.</span></h1>
+        <ul id="items">
+          <li class="project-item">
+            <div class="project-box">
+              <h3>
+                <a class='text2' href="amks-project.php">Ruby on Rails Web App</a>
+              </h3>
+              <a href="amks-project.php">
+                <img src="images/amkarateschool.png">
+              </a>
+            </div>
+          </li>
+          <li class="project-item">
+            <div class="project-box">
+              <h3>
+                <a class='text2'href="arl-project.php">Website Redesign</a>
+              </h3>
+              <a href="arl-project.php">
+                <img src="images/arl-thumbnail.png" alt="thumbnail">
+              </a>
+            </div>
+          </li>
+          <li class="project-item">
+            <div class="project-box">
+              <h3>
+                <a class='text2' href="ukebook-project.php">Visual Booklet</a>
+              </h3>
+              <a href="ukebook-project.php">
+                <img src="images/ukebook-thumbnail.png" alt="thumbnail">
+              </a>
+            </div>
+          </li>
+          <li class="project-item">
+            <div class="project-box">
+              <h3>
+                <a class='text2' href="typespecimen-project.php">Typography</a>
+              </h3>
+              <a href="typespecimen-project.php">
+                <img src="images/typespecimen-thumbnail.png">
+              </a>
+            </div>
+          </li>
+          <li class="project-item">
+            <div class="project-box">
+              <h3>
+                <a class='text2' href="gestalt-project.php">Form &#38; Composition</a>
+              </h3>
+              <a href="gestalt-project.php">
+                <img src="images/gestalt-thumbnail.png" alt="thumbnail">
+              </a>
+            </div>
+          </li>
+        </ul>
+			</div>
+			<br>
+			
+				<div class='text-body text-wrap inside-text'>
+					<h1 id='news-text'>Accomplishments<span>.</span></h1>
+					<img id='bny' src='images/bny.png'>
+          <h2><span>Summer 2013</span>: I spent this past summer working for BNY Mellon as a software development intern. I worked on the web application team where we spent the summer preparing to upgrade the application to the latest version of the software. I created components such as a datagrids with filtering, sorting and other features for the new portal using the Dojo framework. I also gained lots of exposure to server-side Java (JSP) and noSQL databases through development with MongoDB. Check me out in the video below!</h2>
+					<h2><span>Video Coming Soon!</span></h2>
+				</div>
+        <br>
+        
+				<div class="text-wrap inside-text text-body">
+					<h1 id="bio-text">About<span>.</span></h1>
+					<img id='goldengate' src='images/goldengate.png' width='200px'>
+        	<h2>I grew up on the San Francisco Peninsula but I currently live in Pittsburgh, PA as I attend school. My major is in Information Systems and I am taking a minor in Human Computer Interaction, which I am working on turning into a second major. Through my experience as an intern, I have discovered that the proper display of information and development of user-friendly software is essential to a company's success. This is my niche and I want to be able to utilze my skills as a programmer (with an eye for design) to help build better interfaces.</h2>
+				</div> 
+        <br>
+			
+				<div class='text-wrap inside-text text-body'>
+					<h1 id="hobbies-text">Interests<span>.</span></h1>
+          <h2><span>Ukulele &amp; Guitar</span>: The challenge of teaching yourself a skill from scratch is something I find exciting and addicting, which I think is why I started getting into these instruments. Growing up, I was a hip-hop junkie and I never would have expected to fall in love with the Ukulele. I started playing my Freshman year in college and I couldn't stop. Before you know it, I was playing songs by famous Ukulele artists such as <a href='http://www.youtube.com/watch?v=puSkP3uym5k' target='_blank'>Jake Shimabukuro</a> and Troy Fernandez. The following year I bought myself an Alvarez acoustic guitar and I was once again hooked. I've mainly been using my guitar to progress my soloing techniques but I love trying to pick up a song by <a href='http://www.youtube.com/watch?v=AhR04kmcSXU' target='_blank'>Tommy Emmanuel</a> every now and then.</h2>
+				  <h2><span>Sports</span>: Mainly basketball but I love all sports, both watching and playing. My teams/heroes include The Warriors, Giants, 49ers, Novak Djokovic, and Derrick Rose. </h2>
+				  <h2><span>Sneakers</span>: I'm a snearkerhead. My friends in highschool got me into the culture and even though I'm not quite on their level, I can still hold my own. I still catch myself browsing sites like <a href='http://solecollector.com/' target='_blank'>SoleCollector</a> every now and then...</h2>
+			  </div>
+				<br>
+				
+		  </div>
+
+
+<div id='side' class='homeside'>
+  <img src="images/profile.jpg" class="profile" alt="profile-pic">
+  <br><br>
+  <div class="subprofile">
+    <h3>Alexander J. Mark</h3>
+    <h3>CMU Class of 2015</h3>
+		<br><br>
+		<h3 class='scroll' id='portfolio'>Projects</h3>
+		<h3 class='scroll' id='news'>Accomplishments</h3>
+    <h3 class='scroll' id='bio'>About</h3>
+    <h3 class='scroll' id='hobbies'>Interests</h3>
+		<br><br>
+		<h3 class='scroll' id='top'>Top</h3>
+  </div>
+</div>
