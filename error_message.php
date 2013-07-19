69e66dbc6ed77388149004fe0794c99a2d945690
Alex Mark <ajmark@andrew.cmu.edu>
2013-07-19 11:59:18 -0400

    Refactored html head

--- error_message.php
+++ error_message.php
@@ -1,23 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  	<meta charset="utf-8"/>
-  	<title>Alex Mark</title>
-  	<meta name="author" content="Alex Mark" />
-    <meta name="description" content="Personal website for a current Carnegie Mellon student"/>
-  	<meta name="keywords" content="Alex Mark, Carnegie Mellon, Information Systems, cmu, Alexander Mark, website, design, pittsburgh, hillsborough, burlingame, bay area, san francisco, silicon valley, california, pennsylvania, alex, mark, alexander, web design, interaction design, user interface, information architect, information architecture, ajmark, amark"/>
-    <meta name="copyright" content="Alex Mark 2013" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" /> 
-    <link rel="shortcut icon" href="favicon.ico" >
-
-    <!-- Google Web Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,300,700' rel='stylesheet' type='text/css'>
-    
-    <!-- Stylesheets  -->
-    <link rel='stylesheet' type='text/css' href='css/reset.css' />
-    <link rel='stylesheet' type='text/css' href='css/style.css' />
-
-  </head>
+	<?php include('includes/head.php'); ?>
   
   <body>
     <?php include('includes/header.php'); ?>
--- home.php
+++ home.php
@@ -1,29 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  	<meta charset="utf-8"/>
-  	<title>Alex Mark</title>
-  	<meta name="author" content="Alex Mark" />
-    <meta name="description" content="Personal website for a current Carnegie Mellon student"/>
-  	<meta name="keywords" content="Alex Mark, Carnegie Mellon, Information Systems, cmu, Alexander Mark, website, design, pittsburgh, hillsborough, burlingame, bay area, san francisco, silicon valley, california, pennsylvania, alex, mark, alexander, web design, interaction design, user interface, information architect, information architecture, ajmark, amark"/>
-    <meta name="copyright" content="Alex Mark 2013" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <link rel="shortcut icon" href="favicon.ico" >
-
-    <!-- Google Web Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Merriweather+Sans:400,300,700' rel='stylesheet' type='text/css'>
-    
-    <!-- Stylesheets  -->
-    <link rel='stylesheet' type='text/css' href='css/reset.css' />
-    <link rel='stylesheet' type='text/css' href='css/style.css' />
-
-  </head>
-  
+	<?php include('includes/head.php'); ?>
   <body>
-		
     <?php include('includes/header.php'); ?>
 		<?php include('includes/homecontent.php'); ?>
-
     <!-- <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script> -->
     <script src="js/jquery-1.10.1.js"></script>
