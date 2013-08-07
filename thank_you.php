ae6c6817afbef5fc8e14c12c656aa2963f2bcfbb
Alex Mark <ajmark@andrew.cmu.edu>
2013-08-06 23:59:08 -0400

    revisions

--- thank_you.php
+++ thank_you.php
@@ -10,8 +10,6 @@
         <h1>Thank <span>You</span>!</h1>
 				<h1>I'll read your message soon.</h1>
 				<br>
-				<h3>In the meantime check out some cool links!</h3>
-				<br>
 				<h3><a href='index.php'>Click here to go back to the homepage</a></h3>
       </div>
     </div>
